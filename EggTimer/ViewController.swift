9a1fecf1aa7c83c04dfb69fd2fabb2eecfbef09f
AMA2581 <amir.abravesh.ama@gmail.com>
2023-08-21 12:53:58 +0330

    Done

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -6,34 +6,61 @@
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var statusLable: UILabel!
+    @IBOutlet var progressBar: UIProgressView!
     let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
-    
+    var player : AVAudioPlayer!
     var timer = Timer()
+    var totalTime: Float = 0
+    var secondsPassed: Float = 0
 
-    func startTimer(input: Int) {
-        var secondsRemaining = input
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
 
-        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { Timer in
-            if secondsRemaining > 0 {
-                print("\(secondsRemaining) seconds")
-                secondsRemaining -= 1
-            } else {
-                self.statusLable.text = "DONE!"
-                Timer.invalidate()
-            }
+    @objc func updateTimer() {
+        if secondsPassed < totalTime {
+            secondsPassed += 1.0
+            let persentageProgress = secondsPassed / totalTime
+            progressBar.progress = persentageProgress
+        } else {
+            statusLable.text = "DONE!"
+            playSound()
+            timer.invalidate()
         }
+    }
+
+    func playSound() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
     }
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
         let hardness = sender.currentTitle!
-        
-        self.statusLable.text = "How do you like your eggs?"
 
+        statusLable.text = "How do you like your eggs?"
+
+        progressBar.progress = 0.0
+        statusLable.text = hardness
         timer.invalidate()
-        startTimer(input: eggTimes[hardness]!)
+        totalTime = Float(eggTimes[hardness]!)
+        secondsPassed = 0
+        startTimer()
+
+        UIView.animate(withDuration: 0.3) {
+            sender.alpha = 0.5
+        }
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+            UIView.animate(withDuration: 0.3) {
+                sender.alpha = 1.0
+            }
+        }
 
 //        if hardness == "Soft" {
 //            print(softTime)
@@ -54,5 +81,4 @@ class ViewController: UIViewController {
 //            print("error")
 //        }
     }
-    @IBOutlet weak var statusLable: UILabel!
 }
